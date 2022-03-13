fa4d6485057e1c3d7bf69312f2e6ba08d3485260
Owilly <pasaclouma54@gmail.com>
2022-03-13 17:08:41 +0300

    create base file and link i to other pages

--- blog.py
+++ blog.py
@@ -1,3 +1,4 @@
+from turtle import title
 from flask import Flask,render_template
 
 # creating application instance
@@ -6,45 +7,31 @@ app = Flask(__name__)
 # dummy database call
 posts = [
     {
-        'author':'Pascal Owilly',
         'title':'My journey to the wild',
+        'author':'Pascal Owilly',
         'comment':'It was fun',
         'date_posted':'June 11 2022'
     },
     {
-        'author':'Pascal Owilly',
-        'title':'My journey to the wild',
-        'comment':'It was fun',
-        'date_posted':'June 11 2022'
+        'author':'Jane Kameme',
+        'title':'Her journer to somewhere',
+        'comment':'It was cool',
+        'date_posted':'June 20 2022'
     },
 ]
 
 @app.route('/')
+@app.route('/index')
 def index():
 
-    first_name = "Pascal"
-    favourite_food = "Pilau with Chapati"
-    bucketlist = ["Visit Salar de Uyuni","Visit glass bridge to shake a little","Visit bioluminiscence near caribean sea in Jamaica", 75]
-    
-    return render_template('index.html',
-     first_name=first_name,
-     favourite_food=favourite_food,
-      bucketlist= bucketlist,
-      posts=posts)
+    return render_template('index.html', posts = posts)
 
-@app.route('/signin/<name>')
-def signin():
-    return render_template('signin.html')    
+@app.route('/about')
+def about():
 
-# Invalid URL
-app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-# Internal server error
-app.errorhandler(500)
-def page_not_found(e):
-    return render_template('500.html'), 500  
+    title = 'About'
+    return render_template('about.html', title = title)    
+ 
 
 if __name__=='__main__':
     app.run(debug=True)     
